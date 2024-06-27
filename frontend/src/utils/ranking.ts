bb7a5322553dc34bd0333263ecdd4b5cbf393cff
Nattatat <StickyCoffee8360@gmail.com>
2024-06-27 08:46:05 +0700

    frontend - added more mock up scores data.

--- frontend/src/utils/ranking.ts
+++ frontend/src/utils/ranking.ts
@@ -1,19 +1,23 @@
 const ranking = (place: number) => {
   let format_place: string = place.toString();
 
-  switch (format_place.slice(-1)) {
-    case "1":
-      format_place += "st";
-      break;
-    case "2":
-      format_place += "nd";
-      break;
-    case "3":
-      format_place += "rd";
-      break;
-    default:
-      format_place += "th";
-      break;
+  if (format_place.slice(0, 1) === "1") {
+    format_place += "th";
+  } else {
+    switch (format_place.slice(-1)) {
+      case "1":
+        format_place += "st";
+        break;
+      case "2":
+        format_place += "nd";
+        break;
+      case "3":
+        format_place += "rd";
+        break;
+      default:
+        format_place += "th";
+        break;
+    }
   }
 
   return format_place;
